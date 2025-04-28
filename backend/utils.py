2289991f509bb971bc0210b0c7d8f5ddd2e950e7
Arina <a.zimina@innopolis.university>
2025-04-29 00:01:24 +0300

    update param

--- backend/utils.py
+++ backend/utils.py
@@ -14,14 +14,14 @@ def serialize_cluster_state(cluster_state):
     """
     return json.dumps(cluster_state, indent=4)
 
-def simulate_node_failure(probability=0.05):
+def simulate_node_failure(probability=0.01):
     """
     Randomly decide if a node should fail this round, based on a given probability.
     Return True if node should fail.
     """
     return random.random() < probability
 
-def simulate_node_recovery(probability=0.02):
+def simulate_node_recovery(probability=0.05):
     """
     Randomly decide if a sleeping node wakes up this round.
     Return True if node should recover.
